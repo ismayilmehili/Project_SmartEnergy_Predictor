c0e43b0d492ea40f2f2258a490445777ec0eb004
ismayilmehili <ismayilmehili@gmail.com>
2025-03-07 17:29:08 +0100

    Delete unwanted files in production branch

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from app import app
-import os
-
-if __name__ == '__main__':
-    print("🚀 Flask server is starting... Access it at: http://127.0.0.1:5001/")
-    print("Current working directory:", os.getcwd())
-    print("Directory contents:", os.listdir('.'))
-    app.run(host='0.0.0.0', port=5001, debug=True)
